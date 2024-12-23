224ce811f9d61490727171a6b4f9a5bf5261262d
GenKnot <167164229+GenKnot@users.noreply.github.com>
2024-12-23 15:29:15 -0500

    Updated 0.17

--- components/banner.tsx
+++ components/banner.tsx
@@ -5,7 +5,7 @@ import { useTranslations } from 'next-intl';
 export default function Banner() {
     const t = useTranslations('Banner');
     return (
-        <div className="w-full h-52 bg-white flex items-center" style={{ backgroundImage: `url(${'/bkg.jpg'})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+        <div className="w-full h-52 bg-white flex items-center" style={{ backgroundImage: `url(${'/bkg-w.jpg'})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
             <div className="container mx-auto px-4 md:px-6 lg:px-8 xl:px-24">
                 <div className="bg-gray-800/80 py-6 h-3/4 w-full md:w-2/5 flex justify-center text-white flex-col">
                     <h1 className="text-4xl font-bold text-white ml-3">
